5a413fd8bb1c289ffd4d127e83ad48409468d6c9
Ziyaad Labaran <ziyaad.wunintira@dtiafrica.edu.gh>
2025-03-28 09:30:21 +0000

    commit

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 
+document.querySelector('.main-section').addEventListener('mouseenter', function() {
+    this.classList.add('fade-in');
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+ 
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Discover and Experience Ghana!");
+});
+ 
+window.addEventListener("load",() =>{
+const loader = document.querySelector(".loader");
+
+loader.classList.add("loader-hidden");
+
+loader.addEventListener("transitionend",() => {
+document.body.removeChild("loader");
+})
+})
 
